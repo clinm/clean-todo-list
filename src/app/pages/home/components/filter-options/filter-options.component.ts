f89ccfdd9717f041c6ef92f1b3eb85157cb66dd4
Matthieu CLIN <matthieu.clin@outlook.com>
2024-06-14 10:00:33 +0200

    refactor: filter options in own component

---
+++ src/app/pages/home/components/filter-options/filter-options.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatRadioModule } from '@angular/material/radio';
+import { Observable } from 'rxjs';
+import { UpdateTodoOptionsGateway } from '../../../../infra/todo-options/todo-options.gateway';
+import { GetTodoOptionsUsecase } from '../../../../usecases/get-todo-options/get-todo-options.usecase';
+import { TodoOptionVm } from '../../../../usecases/get-todo-options/todo-options.vm';
+
+@Component({
+  selector: 'app-filter-options',
+  standalone: true,
+  imports: [CommonModule, MatRadioModule, ReactiveFormsModule, MatBadgeModule],
+  templateUrl: './filter-options.component.html',
+  styleUrl: './filter-options.component.scss'
+})
+export class FilterOptionsComponent implements OnInit {
+
+    public options$!: Observable<TodoOptionVm>;
+
+    filterForm: FormGroup = new FormGroup({
+      remaining: new FormControl()
+    });
+    
+    constructor(private getTodoOptions: GetTodoOptionsUsecase,
+                private updateTodoOptionsGateway: UpdateTodoOptionsGateway) {}
+  
+    ngOnInit(): void {
+      this.options$ = this.getTodoOptions.run();
+  
+      this.initFilterListener();
+  
+    }
+  
+    private initFilterListener() {
+      this.options$
+            .subscribe(option => {
+              this.filterForm.patchValue(option, { emitEvent: false});
+            });
+  
+      this.filterForm.valueChanges
+          .subscribe(form => {
+            this.updateTodoOptionsGateway.update(form);
+          });
+    }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,58 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { MatBadgeModule } from '@angular/material/badge';
 import { MatCardModule } from '@angular/material/card';
-import { MatRadioModule } from '@angular/material/radio';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Observable } from 'rxjs';
-import { UpdateTodoOptionsGateway } from '../../infra/todo-options/todo-options.gateway';
 import { GetTodoListUsecase } from '../../usecases/get-todo-list/get-todo-list.usecase';
 import { TodoListVM, TodoListViewModelType } from '../../usecases/get-todo-list/todo-list.vm';
-import { GetTodoOptionsUsecase } from '../../usecases/get-todo-options/get-todo-options.usecase';
-import { TodoOptionVm } from '../../usecases/get-todo-options/todo-options.vm';
 import { DisplayListTodoComponent } from './components/display-list-todo/display-list-todo.component';
+import { FilterOptionsComponent } from "./components/filter-options/filter-options.component";
 
 @Component({
-  selector: 'home-root',
-  standalone: true,
-  imports: [MatToolbarModule, CommonModule, MatCardModule, MatRadioModule, DisplayListTodoComponent, ReactiveFormsModule, MatBadgeModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'home-root',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [MatToolbarModule, CommonModule, MatCardModule, DisplayListTodoComponent, FilterOptionsComponent]
 })
 export class HomeComponent implements OnInit {
 
   public todos$!: Observable<TodoListVM>;
 
-  public options$!: Observable<TodoOptionVm>;
-
   TodoListViewModelType = TodoListViewModelType;
-
-  filterForm: FormGroup = new FormGroup({
-    remaining: new FormControl()
-  });
   
-  constructor(private getTodoList: GetTodoListUsecase,
-              private getTodoOptions: GetTodoOptionsUsecase,
-              private updateTodoOptionsGateway: UpdateTodoOptionsGateway) {}
+  constructor(private getTodoList: GetTodoListUsecase) {}
 
   ngOnInit(): void {
-    this.options$ = this.getTodoOptions.run();
     this.todos$ = this.getTodoList.run();
-
-    this.initFilterListener();
-
   }
 
-  private initFilterListener() {
-    this.options$
-          .subscribe(option => {
-            this.filterForm.patchValue(option, { emitEvent: false});
-          });
-
-    this.filterForm.valueChanges
-        .subscribe(form => {
-          this.updateTodoOptionsGateway.update(form);
-        });
-  }
 }
